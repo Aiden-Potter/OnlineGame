e316f97e7c70fda1955b9347334096cb0010da1d
ziming <2834691652@qq.com>
2019-12-29 23:19:32 +0800

    坦克逻辑

--- Test/TankDemo/Assets/7Sound/SoundPlayer.cs
+++ Test/TankDemo/Assets/7Sound/SoundPlayer.cs
@@ -6,7 +6,7 @@ public class SoundPlayer : MonoBehaviour
     void OnGUI()
     {
         AudioSource audio = GetComponent<AudioSource>();
-        if (GUI.Button(new Rect(0, 0, 100, 50), "开始"))
+        if (GUI.Button(new Rect(1820, 0, 100, 50), "开始"))
             audio.Play();
         if (GUI.Button(new Rect(100, 0, 100, 50), "停止"))
             audio.Stop();
