7117211758a13030bfa800ab2192e7be825faf7a
ziming <2834691652@qq.com>
2020-01-11 00:38:41 +0800

    坦克的轮子+日志

---
+++ Test/TankDemo/Assets/Scripts/AxleInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AXleInfo
+{
+    //
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    //两个车轮碰撞器
+    public bool motor;//是否有马力传达，控制前驱后驱
+    public bool steering;//前轮转向
+}
