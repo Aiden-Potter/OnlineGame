aa88ce123abbb46872d752d17cb9470c948e8f63
ziming <2834691652@qq.com>
2020-01-18 06:39:06 +0800

    1111

---
+++ Test/TankDemo/Assets/VisualEffectContrl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+public class VisualEffectContrl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public VisualEffect ve;
+    void Start()
+    {
+        ve.SendEvent("OnStop");
+;   }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+           {
+            ve.SendEvent("OnPlay");
+        }
+
+    }
+}
