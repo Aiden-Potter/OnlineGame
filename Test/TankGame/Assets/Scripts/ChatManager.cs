a0f3c5540158d58253213a1421faa3e20ce68487
ziming <2834691652@qq.com>
2020-02-17 16:09:58 +0800

    聊天室

---
+++ Test/TankGame/Assets/Scripts/ChatManager.cs
@@ -0,0 +1,105 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ChatManager : MonoBehaviour
+{
+
+
+    public string ipaddress = "192.168.137.1";//服务器ip
+    public int port = 7799;
+    public InputField MessageInput;
+    public Text MessageText;
+
+    private Socket clientSocket;
+    private Thread thread;
+    private byte[] data = new byte[1024];// 数据容器
+    private string message = "";
+
+    void Start()
+    {
+        
+        ConnectToServer();
+    }
+
+    void Update()
+    {
+        //只有在主线程才能更新UI
+        if (message != "" && message != null)
+        {
+            MessageText.text += "\n" + message;
+            message = "";
+        }
+        Debug.Log(getLocalIP());
+    }
+    /**
+     * 连接服务器端函数
+     * */
+    void ConnectToServer()
+    {
+        clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        //跟服务器连接
+       // clientSocket.Connect(new IPEndPoint(IPAddress.Parse(ipaddress), port));
+         IPAddress ipAddress = IPAddress.Parse("192.168.137.1");
+
+         //Debug.Log(Dns.GetHostEntry(ipaddress).AddressList[4].ToString());
+         IPEndPoint serverEndPoint = new IPEndPoint(ipAddress,port);
+         clientSocket.Connect(serverEndPoint);
+        //客户端开启线程接收数据
+        thread = new Thread(ReceiveMessage);
+        thread.Start();
+
+    }
+
+    void ReceiveMessage()
+    {
+        while (true)
+        {
+            if (clientSocket.Connected == false)//客户端掉线了
+            {
+                break;
+            }
+            int length = clientSocket.Receive(data);
+            message = Encoding.UTF8.GetString(data, 0, length);
+            print(message);
+        }
+
+    }
+
+    void SendMessage(string _message)
+    {
+        byte[] data = Encoding.UTF8.GetBytes(_message);
+        clientSocket.Send(data);
+    }
+
+    public void OnSendButtonClick()
+    {
+  
+        string value = System.DateTime.Now.Hour.ToString()+":"+System.DateTime.Now.Minute +" "+ MessageInput.text;
+        SendMessage(value);
+        MessageInput.text = " ";//更新inputField
+    }
+    /**
+     * unity自带方法
+     * 停止运行时会执行
+     * */
+    void OnDestroy()
+    {
+        //关闭连接，分接收功能和发送功能，both为两者均关闭
+        clientSocket.Shutdown(SocketShutdown.Both);
+        clientSocket.Close();
+    }
+
+    public string getLocalIP()
+    {
+        string strHostName = Dns.GetHostName(); //得到本机的主机名
+        IPHostEntry ipEntry = Dns.GetHostEntry(strHostName); //取得本机IP，
+        string strAddr = ipEntry.AddressList[4].ToString();//取出ipv4地址  list的所有ip，包括ipv6、ipv4
+        return (strAddr);
+    }
+
+}
---
+++ Test/TankGame/Assets/Scripts/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCP_Sever_Unity
+{
+    class Program
+    {
+        static List<Client> clientlists = new List<Client>();
+        /// <summary>
+        /// 广播信息
+        /// </summary>
+        /// <param name="message"></param>
+        public static void BroadcastMessage(string message)
+        {
+            var NotConnectClient = new List<Client>();//掉线线客户端集合
+            foreach (var client in clientlists)
+            {
+                if (client.Connected())//判断是否在线
+                {
+                    client.SendMessage(message);
+                }
+                else
+                {
+                    NotConnectClient.Add(client);
+                }
+            }
+            //将掉线的客户端从集合里移除
+            foreach (var temp in NotConnectClient)
+            {
+                clientlists.Remove(temp);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Socket tcpServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipAddress = IPAddress.Parse("192.168.0.102");
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, 7799);
+            tcpServer.Bind(ipEndPoint);
+
+            Console.WriteLine("服务器开启....");
+            tcpServer.Listen(100);
+            //循环，每连接一个客户端建立一个Client对象
+            while (true)
+            {
+                Socket clientSocket = tcpServer.Accept();//暂停等待客户端连接，连接后执行后面的代码
+                Client client = new Client(clientSocket);//连接后，客户端与服务器的操作封装到Client类中
+                Console.WriteLine("一个客户端连接....");
+                clientlists.Add(client);//放入集合
+            }
+        }
+    }
+}
